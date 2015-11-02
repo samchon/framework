e0a65d8650d52e2a4a319022e739ee8475fae262
Jun Ryung Ju <jjr0502@naver.com>
2015-11-02 19:21:36 +0900

    now MSBuild use multi threads for build

--- build.bat
+++ build.bat
@@ -3,11 +3,11 @@ call "%VS140COMNTOOLS%\VsDevCmd.bat"
 :: ---------------------------------------------------------------------------------
 ::	Build C++ Projects
 :: ---------------------------------------------------------------------------------
-MSBuild cpp\SamchonFramework.sln /property:Configuration=Debug /property:Platform=x86 /target:SamchonFramework
-MSBuild cpp\SamchonFramework.sln /property:Configuration=Release /property:Platform=x86 /target:SamchonFramework
+MSBuild cpp\SamchonFramework.sln /property:Configuration=Debug /m /property:Platform=x86 /target:SamchonFramework
+MSBuild cpp\SamchonFramework.sln /property:Configuration=Release /m /property:Platform=x86 /target:SamchonFramework
 
-MSBuild cpp\SamchonFramework.sln /property:Configuration=Debug /property:Platform=x64 /target:SamchonFramework
-MSBuild cpp\SamchonFramework.sln /property:Configuration=Release /property:Platform=x64 /target:SamchonFramework
+MSBuild cpp\SamchonFramework.sln /property:Configuration=Debug /m /property:Platform=x64 /target:SamchonFramework
+MSBuild cpp\SamchonFramework.sln /property:Configuration=Release /m /property:Platform=x64 /target:SamchonFramework
 
 :: ---------------------------------------------------------------------------------
 ::	Build JS Projects

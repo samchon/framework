f563cc6b0f26bb11af5692352506c86f16724439
Jeongho Nam <samchon@samchon.org>
2015-12-23 15:15:11 +0900

    12/23/2015 Wed 15:15:11.43

--- samchon_framework_dll.bat
+++ samchon_framework_dll.bat
@@ -3,3 +3,5 @@ SET SAMCHON_DIR=D:\OneDrive\Project\Samchon\framework\cpp\
 xcopy %SAMCHON_DIR%*.hpp /s /Y
 xcopy %SAMCHON_DIR%*.lib /s /Y
 xcopy %SAMCHON_DIR%*.dll /s /Y
+
+del samchon\documentation\mainpage.hpp

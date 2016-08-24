9ba0347e06c048bb200098120e5830aade970985
이두영 <leeduyoung20@naver.com>
2016-08-24 22:06:31 +0900

    * chat-application-ui 최초등록

---
+++ ts/examples/chat-application-ui/src/API.ts
@@ -0,0 +1,8 @@
+﻿namespace example.chat
+{
+	export import library = samchon.library;
+	export import protocol = samchon.protocol;
+
+	export const SERVER_HOST: string = "127.0.0.1"; //"115.71.237.198";
+	export const SERVER_PORT: number = 11723;
+}
---
+++ ts/examples/chat-application-ui/src/Application.tsx
@@ -0,0 +1,64 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+namespace example.chat
+{
+	export abstract class Application
+		extends React.Component<{}, {}>
+		implements protocol.IProtocol
+	{
+		protected id: string;
+		protected name: string;
+
+		protected communicator: protocol.WebServerConnector;
+
+		public constructor()
+		{
+			super();
+		}
+
+		/* =========================================================
+			INVOKE MESSAGE CHAIN
+				- SEND DATA
+				- REPLY DATA
+		============================================================
+			SEND DATA
+		--------------------------------------------------------- */
+		public sendData(invoke: protocol.Invoke): void
+		{
+			this.communicator.sendData(invoke);
+		}
+
+		/* ---------------------------------------------------------
+			REPLY DATA
+		--------------------------------------------------------- */
+		public replyData(invoke: protocol.Invoke): void
+		{
+			invoke.apply(this);
+		}
+
+		protected setAccount(id: string, name: string): void
+		{
+			this.id = id;
+			this.name = name;
+
+			this.refresh();
+		}
+
+		private alert(message: string): void
+		{
+			alert(message);
+		}
+
+		/* ---------------------------------------------------------
+			VISUALIZER
+		--------------------------------------------------------- */
+		public abstract render(): JSX.Element;
+
+		protected refresh(): void
+		{
+			ReactDOM.render(this.render(), document.body);
+		}
+	}
+}

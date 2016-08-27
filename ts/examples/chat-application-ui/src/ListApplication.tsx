5369bdc01bd12e998ea867790f2773186b9054ab
dabin <valve89@naver.com>
2016-08-28 00:38:12 +0900

    * 채팅방을 새 창이 아닌 현재 창 탭으로 추가하도록 변경

--- ts/examples/chat-application-ui/src/ListApplication.tsx
+++ ts/examples/chat-application-ui/src/ListApplication.tsx
@@ -9,6 +9,9 @@ namespace example.chat {
 		extends Application {
 		private room_list: ChatRoomList;
 
+		private room_name_tabs: JSX.Element[] = [];  /* 채팅방 탭 제목 배열 */
+		private room_tabs: JSX.Element[] = [];       /* 채팅방 탭 내용 배열 */
+
 		/* ---------------------------------------------------------
 			CONSTRUCTORS
 		--------------------------------------------------------- */
@@ -57,7 +60,29 @@ namespace example.chat {
 			this.refresh();
 		}
 
-		/* ---------------------------------------------------------
+		/*----------------------------------------------------
+			채팅방 탭 추가
+		----------------------------------------------------*/
+		private enter_chat_room(uid: number, title: string) {
+
+			let chatRoomUrl: string = "chat.html?&uid=" + uid;
+
+			let room_name_link: string = "#" + uid;
+
+			this.room_name_tabs.push(
+				<li><a data-toggle="tab" href={room_name_link}>{title}</a></li>
+			)
+
+			this.room_tabs.push(
+				<div id={uid} className="tab-pane fade">
+					<iframe src={chatRoomUrl} height="100%" width="100%" />
+				</div>
+			)
+
+			this.refresh();
+		}
+
+	/* ---------------------------------------------------------
 			VISUALIZER
 		--------------------------------------------------------- */
 		public render(): JSX.Element {
@@ -80,7 +105,7 @@ namespace example.chat {
 
 				room_elements.push
 					(
-					<p><a href={link_address} target="_blank">
+					<p>
 						<table>
 							<tr>
 								<td> No </td>
@@ -98,38 +123,63 @@ namespace example.chat {
 									</ol>
 								</td>
 							</tr>
+							<tr>
+								<td>
+									<button onClick={() => this.enter_chat_room(room.getUID(), room.getTitle()) }>참여</button>
+								</td>
+							</tr>
 						</table>
-					</a></p>
+					</p>
 					);
 			}
 
 			return <div className="container">
 
-				<div className="sidebar">
-					<div className="talk-title">
-						<h1>✿ 삼촌톡 ✿</h1>
-					</div>
+				<div class="row">
+					<div className="col-sm-3 col-md-2 sidebar">
+						<div className="talk-title">
+							<h1>✿ 삼촌톡 ✿</h1>
+						</div>
 
-					<div className="create-room">
-						<h2> Create Room </h2>
-						<input id="create_room_input" type="text" />
-						<button onClick={this.create_room.bind(this) }>Create</button>
-					</div>
+						<div className="create-room">
+							<h2> Create Room </h2>
+							<input id="create_room_input" type="text" />
+							<button onClick={this.create_room.bind(this) }>Create</button>
+						</div>
 
-					<div className="chat-room-list">
-						<h2> List of Chatting Room </h2>
-						{room_elements}
-					</div>
+						<div className="chat-room-list">
+							<h2> List of Chatting Room </h2>
+							{room_elements}
+						</div>
 
-					<div className="user-info">
-						<h2> User Information </h2>
-						<ul>
-							<li> Account ID: {this.id} </li>
-							<li> Name: {this.name} </li>
-						</ul>
+						<div className="user-info">
+							<h2> User Information </h2>
+							<ul>
+								<li> Account ID: {this.id} </li>
+								<li> Name: {this.name} </li>
+							</ul>
+						</div>
+					</div>
+					<div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+						<div className="container">
+							<ul className="nav nav-tabs">
+								<li className="active"><a data-toggle="tab" href="#home">Home</a></li>
+								{this.room_name_tabs}
+							</ul>
+							<div className="tab-content">
+								<div id="home" className="tab-pane fade in active">
+									<img src="https://avatars2.githubusercontent.com/u/13158709?v=3&s=300"></img>
+									<h2>삼촌톡에 오신걸 환영합니다.</h2>
+									<p><h3>왼쪽 메뉴에서 채팅방을 만든 후 '참여' 버튼을 눌러 채팅을 즐기세요!</h3></p>
+								</div>
+								{this.room_tabs}
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>;
+			</div>
+				;
 		}
 
 		public static main(): void {
